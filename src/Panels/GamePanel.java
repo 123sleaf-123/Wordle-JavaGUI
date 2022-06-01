41e79f829119be564f1e539daea9c6a57a5fbdae
sleaf <huangzhiye2002@163.com>
2022-06-01 19:54:44 +0800

    现在在主界面GamePanel会初始化一个InputFilter成员，并且把键盘输入传递给该成员的inputProcess方法来决定弹出错误的类型或者填入单词。

--- src/Panels/GamePanel.java
+++ src/Panels/GamePanel.java
@@ -6,6 +6,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import Components.JStyleLabel;
+import Supportor.InputFilter;
 
 /**
  * Title:        GamePanel.java
@@ -16,6 +17,8 @@ import Components.JStyleLabel;
  * @description The panel for the game, extending from JPanel.
  **/
 public class GamePanel extends JPanel implements KeyListener {
+    private InputFilter inputFilter;
+
     private JStyleLabel[][] table = new JStyleLabel[6][5];
     private int size = 5;
 
@@ -33,7 +36,7 @@ public class GamePanel extends JPanel implements KeyListener {
 //        System.out.println("Pressed");
 //        System.out.println(e.getKeyChar());
         System.out.println("Pressed " + e.getKeyChar() + '\n' + this);
-
+        inputFilter.inputProcess(e.getKeyChar());
     }
 
     @Override
@@ -45,6 +48,7 @@ public class GamePanel extends JPanel implements KeyListener {
      * Initialisation of GamePanel class
      */
     public void initPanel() {
+        inputFilter = new InputFilter(size);
         this.addKeyListener(this);
         this.setLayout(new GridLayout(6, 5, 1, 1));
         for (int i = 0; i < 6; i++) {
